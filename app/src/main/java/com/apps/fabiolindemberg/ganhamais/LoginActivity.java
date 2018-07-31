9ebe507952ec4c9b75623adebc0bf08f8902fa9f
Fábio Lindemberg <fabiolindemberg>
2018-07-31 12:12:16 -0300

    adding StatementActivity

--- app/src/main/java/com/apps/fabiolindemberg/ganhamais/LoginActivity.java
+++ app/src/main/java/com/apps/fabiolindemberg/ganhamais/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.apps.fabiolindemberg.ganhamais;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnGetIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnGetIn = findViewById(R.id.btnEntrar);
+
+        btnGetIn.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+                                            Intent intent = new Intent(view.getContext(), MainActivity.class);
+                                            startActivity(intent);
+                                        }
+                                    }
+        );
     }
 }
